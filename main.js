3894e98bce4bd7bf4780a3b3b56f1e24e449bb9d
Tyler Wong <tylerwong.j@gmail.com>
2017-12-02 00:39:40 -0800

    header styling and tons of changes

---
+++ main.js
@@ -0,0 +1,16 @@
+var content = document.querySelector('.js-layout__content');
+
+function resizeHeaderOnScroll() {
+	var distanceY = this.pageYOffset || this.scrollTop;
+	var distanceShrink = 100;
+	var header = document.querySelector('.js-header');
+
+	if (distanceY > distanceShrink) {
+		header.classList.add("is-small");
+	}
+	else {
+		header.classList.remove("is-small");
+	}
+}
+
+content.addEventListener('scroll', resizeHeaderOnScroll);
